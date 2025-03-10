44d4990c0c4b7b61bc8a91428bf4e9d1688268d7
aysu <aysu.aliyeva.211@gmail.com>
2025-03-11 01:26:39 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*for (int i = 1; i < 100; i++)
+{
+    if (i%2==0)
+    {
+        Console.WriteLine(i);
+    }
+
+}*/
+/*int b = 1;
+while (b <= 100)
+{
+    if (b % 2 == 0)
+    Console.WriteLine(b);
+    b++;
+}
+*/
